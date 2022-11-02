14db36044a3d541742d87fee43604542d168e2fd
Arinze Cyril <61941210+ArinzeCyril@users.noreply.github.com>
2022-11-02 06:16:15 +0100

    FIRST COMMIT; APP COMPONENT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfilePix from './img/profile__img.png';
+import SlackPix from './img/slack.png';
+import GithubPix from './img/github.png';
+import ZuriPix from './img/zuri.png';
+import I4GPix from './img/I4G.png';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <main>
+        <img id="profile_img" src={ProfilePix} alt="profile" />
+        <h2 id='profile_text'>Annette Black</h2>
 
-export default App;
+        <p id="twitter">Twitter Link</p>
+        <p id="slack">ArinzeCyril</p>
+        {/* <div className="a">dfgdf</div> */}
+        <a href="https://training.zuri.team/" id="btn_zuri">Zuri Team</a>
+        <a href="http://books.zuri.team" id="books">Zuri Books <sub>This is where you will find books on design and coding</sub></a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=ArinzeCyril" id="book_python">Python Books <sub>Do you wish to learn Python? this is the best place to start</sub></a>
+        <a href="https://background.zuri.team" id="pitch">Backgroung Check for Coders <sub>Let us run a background check on your coders</sub></a>
+        <a href="https://books.zuri.team/design-rules" id="book__design">Design Books <sub>Are you passionate about design but dont know where to start? Here's a free design ebook offered by Zuri</sub></a>
+        <div id='SM'>
+          <img src={SlackPix} alt="slack" />
+          <img src={GithubPix} alt="github" />
+        </div>
+      </main>
+
+      <footer>
+        <img id='zuripix' src={ZuriPix} alt="Zuri" />
+        <p>HNG Internship 9 Frontend Task</p>
+        <img id='i4gpix' src={I4GPix} alt="I4g" />
+      </footer>
+    </>
+  )
+}
